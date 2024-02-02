29436b3aa6c205545eb5151e21a2be0229be48eb
jesseturner21 <jesse.turner2021@gmail.com>
2024-02-02 09:57:43 -0700

    login functionality

--- Login/urls.py
+++ Login/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+#urls for app login
 urlpatterns=[
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("login/", views.login_view, name="login")
 ]
